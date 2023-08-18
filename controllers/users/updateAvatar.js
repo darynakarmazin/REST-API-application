3a8ca25da60c46f0d8d2c09861696e0ae3817ce7
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-08-18 12:09:53 +0300

    Step 4 Added avatarURL with unique name

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -7,11 +7,13 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res, next) => {
   const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const imageName = `${id}_${originalname}`;
   try {
-    const resultUpload = path.join(avatarsDir, originalname);
+    const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("public", "avatars", originalname);
-    await User.findByIdAndUpdate(req.urer._id, { avatarURL });
+    const avatarURL = path.join("public", "avatars", imageName);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.status(200).json({
       status: "OK",
       code: 200,
