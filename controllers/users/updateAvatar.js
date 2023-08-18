df01ddd31b6eb8206cbfff98757df48980cf4f57
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-08-18 11:50:57 +0300

    Step 5 Added endpoint /users/avatars

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,3 +1,27 @@
-const updateAvatar = async (req, res, next) => {};
+const { User } = require("../../models");
+
+const path = require("path");
+const fs = require("fs/promises");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res, next) => {
+  const { path: tempUpload, originalname } = req.file;
+  try {
+    const resultUpload = path.join(avatarsDir, originalname);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("public", "avatars", originalname);
+    await User.findByIdAndUpdate(req.urer._id, { avatarURL });
+    res.status(200).json({
+      status: "OK",
+      code: 200,
+        avatarURL,
+      },
+    });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    next(error);
+  }
+};
 
 module.exports = updateAvatar;
